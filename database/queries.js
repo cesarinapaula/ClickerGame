2e614377beedd5ec70f408a0e7e9bfe994de07d5
Ivan <imendoza9691@gmail.com>
2018-02-11 16:10:24 -0500

    "added queries"

---
+++ database/queries.js
@@ -0,0 +1,81 @@
+var pgp = require("pg-promise")({});
+var connectionString = "postgres://localhost/userlist";
+var db = pgp(connectionString);
+const authHelpers = require("../auth/helpers");
+const passport = require("../auth/local");
+
+function getAllUsers(req, res, next) {
+  db
+    .any("select * from users")
+    .then(function(data) {
+      res.status(200).json({
+        status: "success",
+        data: data,
+        message: "Retrieved ALL users"
+      });
+    })
+    .catch(function(err) {
+      return next(err);
+    });
+}
+
+function getSingleUser(req, res, next) {
+  db
+    .any("select * from users where username = ${username}", req.params)
+    .then(function(data) {
+      res.status(200).json({
+        status: "success",
+        data: data,
+        message: "Fetched one user"
+      });
+    })
+    .catch(function(err) {
+      return next(err);
+    });
+}
+
+function updateSingleUser(req, res, next) {
+  db
+    .none(
+      "update users set username = ${newName} where username = ${username}",
+      req.body
+    )
+    .then(function(data) {
+      res.status(200).json({
+        status: "success",
+        message: "Changed one user"
+      });
+    })
+    .catch(function(err) {
+      return next(err);
+    });
+}
+
+function registerUser(req, res, next) {
+  return authHelpers
+    .createUser(req)
+    .then(response => {
+      passport.authenticate("local", (err, user, info) => {
+        if (user) {
+          res.status(200).json({
+            status: "success",
+            data: user,
+            message: "Registered one user"
+          });
+        }
+      })(req, res, next);
+    })
+    .catch(err => {
+      res.status(500).json({
+        status: "error",
+        error: err
+      });
+    });
+}
+
+module.exports = {
+  getAllUsers: getAllUsers,
+  getSingleUser: getSingleUser,
+  registerUser: registerUser,
+  updateSingleUser: updateSingleUser
+};
